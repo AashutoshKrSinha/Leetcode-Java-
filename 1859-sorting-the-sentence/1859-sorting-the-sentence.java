33e5ea52d078d7af7272b8fb1fa181b9d7871811
AashutoshKrSinha <131376250+AashutoshKrSinha@users.noreply.github.com>
2026-09-01 11:47:04 +0530

    Time: 0 ms (100%), Space: 43 MB (12.69%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] res = new String[words.length];
+
+        for (String word : words) {
+            int pos = word.charAt(word.length() - 1)- '0';
+            String actualWord = word.substring(0, word.length() - 1);
+            res[pos - 1] = actualWord;
+        }
+
+        return String.join(" ", res);
+    }
+}
