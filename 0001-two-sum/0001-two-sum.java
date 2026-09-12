5366f8068f839b7f558c5821897af5b6cf8322d5
AashutoshKrSinha <131376250+AashutoshKrSinha@users.noreply.github.com>
2026-09-12 14:46:18 +0530

    Time: 45 ms (26.91%), Space: 47 MB (59.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,9 +5,7 @@ class Solution {
 
                 if(nums[i] + nums[j] == target){
                     return new int []{i , j};
-
-                }
-                
+                } 
             }
         }
         return new int [] {};
